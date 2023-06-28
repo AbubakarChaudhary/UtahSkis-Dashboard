1700f430b152a18201d550dbd0d0a2bcbcd0473a
AbubakarChaudhary <mahboobabubakar786@gmail.com>
2023-06-28 21:44:48 +0500

    Update Dashboard.jsx

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -7,14 +7,14 @@ const Content = (props) => {
   return (
     <>
       <div className="flex flex-wrap">
-        <div className="md:w-3/4 pr-5">
+        <div className="w-full md:w-3/4 pr-5">
           <h1 className="text-2xl font-bold mb-7 text-gray-800">
             {props.heading}
           </h1>
           <Card />
           <Table />
         </div>
-        <div className="md:w-1/4  ">
+        <div className="md:w-1/4">
           <h1 className="text-2xl font-bold mb-7 text-gray-800">Latest News</h1>
           <div className="bg-gray-50 p-5">
             <LatestNews />
